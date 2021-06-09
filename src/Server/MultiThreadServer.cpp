fb06782247fc31460e724e80bbb168ff961fa068
ernestchu <b073040018@nsysu.edu.tw>
2021-06-09 21:38:17 +0800

    remove select in multi-thread

--- src/Server/MultiThreadServer.cpp
+++ src/Server/MultiThreadServer.cpp
@@ -10,37 +10,30 @@ namespace srv {
         unsigned int alen;          // length of client's address
         int msock;                  // master server socket
         int ssock;                  // slave server socket
-        fd_set rfds;                // read file descriptor set
 
         std::vector<std::future<void>> futures; // vector of thread's future
 
         msock = cnt::passiveTCP(this->service, this->QLEN, this->log);
-        FD_ZERO(&rfds);
-        FD_SET(msock, &rfds);
         this->mu.lock();
         std::cout << "Master thread ID: " << std::this_thread::get_id() << std::endl;
         this->mu.unlock();
 
         while (1) {
-            if (select(msock+1, &rfds, (fd_set*)0, (fd_set*)0, (struct timeval*)0) < 0)
-                cnt::errexit("select error: %s\n", strerror(errno));
-            
-            if (FD_ISSET(msock, &rfds)) {
-                // accept new connection
-                alen = sizeof(sin);
-                ssock = accept(msock, (struct sockaddr *)&sin, &alen);
-                if (ssock < 0) {
-                        cnt::errexit("accept: %s\n", strerror(errno));
-                }
-                this->addNewClient(sin, ssock);
+	    // accept new connection
+	    alen = sizeof(sin);
+	    ssock = accept(msock, (struct sockaddr *)&sin, &alen);
+	    if (ssock < 0) {
+		    cnt::errexit("accept: %s\n", strerror(errno));
+	    }
+	    this->addNewClient(sin, ssock);
 
-                // make a new thread to handle new client
-                futures.emplace_back(
-                    std::async(std::launch::async, [ssock, this] {
-                        this->connectionHandler(ssock);
-                    })
-                );
-            }
+	    // make a new thread to handle new client
+	    futures.emplace_back(
+		std::async(std::launch::async, [ssock, this] {
+		    this->connectionHandler(ssock);
+		})
+	    );
+
             // clean the completed thread
             for (auto it = futures.begin(); it != futures.end(); ) {
                 if (it->wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
